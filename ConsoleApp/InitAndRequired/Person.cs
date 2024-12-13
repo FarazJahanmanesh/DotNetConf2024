8650d7053754e05d373edf9fb7b8254d2e31ad9c
faraz jahan manesh <farazebipax@gmail.com>
2024-12-13 19:31:37 +0330

    Init And Required

---
+++ ConsoleApp/InitAndRequired/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp.InitAndRequired;
+public class Person
+{
+    public string Name { get; set; }
+    //public required string Name { get; set; }
+    //public string Name { get; init; }
+    //public required string Name { get; init; }
+}
