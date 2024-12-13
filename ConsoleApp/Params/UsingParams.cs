d68ece25c4571f08fa125b55d00c5bf3eab6980a
faraz jahan manesh <farazebipax@gmail.com>
2024-12-13 16:48:46 +0330

    using params and the changes

---
+++ ConsoleApp/Params/UsingParams.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp.Params;
+public class UsingParams
+{
+    public void PrintItems(params IEnumerable<int>[] collections)
+    {
+        foreach (IEnumerable<int> collection in collections)
+        {
+            foreach (int item in collection)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    //    کلمه کلیدی params برای تعریف متود هایی با تعداد ورودی متغیر استفاده می شود
+    //و هر متود تنها اجازه دارد یک ورودی از این نوع داشته باشد اگر موقع فراخوانی مقداری داده نشود
+    //به عنوان ارایه خالی در نظر گرفته میشود و باید اخرین ورودی متود باشد
+
+    //public void Concat<T>(params ReadOnlySpan<T> items, params ReadOnlySpan<T> items2 , string item3)
+    //{
+    //    for (int i = 0; i < items.Length; i++)
+    //    {
+    //        Console.Write(items[i]);
+    //        Console.Write(" ");
+    //    }
+    //    Console.WriteLine();
+    //}
+
+    //نحوه تعریف در سی شارپ 12
+    //    در سی شارپ 12 فقط آرایه ها را میپذیرفت
+    //یعنی مجبور بودیم سایر collection ها را به آرایه تغییر دهیم
+
+    public void ConcatenateStrings(params string[] strings)
+    {
+        string result = string.Join(" ", strings);
+        Console.WriteLine(result);
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,19 @@
-﻿namespace DotNetConf2024.CSharp13.ConsoleApp;
+﻿using ConsoleApp.Params;
+
+namespace DotNetConf2024.CSharp13.ConsoleApp;
 public class Program
 {
     public static void Main(string[] args)
     {
+        UsingParams usingParams = new UsingParams();
+
+        //در سی شارپ 12 باید همه تایپ ها به ارایه تبدیل میکردیم
+        var list = new List<int> { 1, 2, 3 };
+        var array = new int[] { 4, 5, 6 };
+        var queue = new Queue<int>(new[] { 7, 8 });
+        var set = new HashSet<int> { 9, 10 };
+
+        usingParams.PrintItems(list, array, queue, set);
+
     }
 }
